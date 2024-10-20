8a75c9a8a62fcb7ec2547d5f97afb3997486b08f
Torao <bahoann79@gmail.com>
2024-10-20 23:45:31 +0700

    ui: add checkout and review box component

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { StarsReview } from "../Utils/StarsReview";
+import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
 
 export const BookCheckoutPage = () => {
 
@@ -80,7 +81,8 @@ export const BookCheckoutPage = () => {
 
                         </div>
                     </div>
-                </div> 
+                    <CheckoutAndReviewBox book={book} mobile={false} />
+                </div>
                 <hr />
             </div>
 
@@ -101,6 +103,7 @@ export const BookCheckoutPage = () => {
                         <StarsReview rating={4.5} size={32} />
                     </div>
                 </div>
+                <CheckoutAndReviewBox book={book} mobile={true} />
                 <hr />
             </div>
         </div>
